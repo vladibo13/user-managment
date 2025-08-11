83049eb2d7a747fb3c574219509def78ddec0d0f
vladibo13 <vladibo13@gmail.com>
2025-08-11 12:13:06 +0300

    added mongo db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || "";
 
 app.use(express.json());
 
@@ -12,6 +14,8 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express + TypeScript!");
 });
 
-app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+connectDB(MONGO_URI).then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
 });
